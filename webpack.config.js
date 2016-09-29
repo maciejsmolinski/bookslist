c978e4000903209c4380ad2b6df0e750cc775452
Maciej Smolinski <contact@maciejsmolinski.com>
2016-09-30 00:00:07 +0200

    Workflow: Use babel loaders to transpile ES6/ES2015 syntax to ES5.
    By using webpack babel loaders the pool of target browsers increase a lot
    because the modern syntax gets transpiled to the older syntax
    (e.g. arrow functions are morphed into regular functions)
    
    Since now more browsers will be able to run the application, they know ES5 syntax already.

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,18 @@ module.exports = {
   output: {
     filename: './dest/app.js',
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            'es2015',
+          ],
+        },
+      },
+    ],
+  },
 };

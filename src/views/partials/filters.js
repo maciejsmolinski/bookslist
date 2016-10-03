477a339a35a623bfed7be2c8c75f7cedb51c248c
Maciej Smolinski <contact@maciejsmolinski.com>
2016-10-03 23:19:47 +0200

    Filters: Change the order of filters so they're more intuitive.

--- src/views/partials/filters.js
+++ src/views/partials/filters.js
@@ -24,31 +24,6 @@ module.exports = (filters, send) => {
       <div class="filters__item">
         <label>
           <strong class="filters__label">
-            Sort by:
-          </strong>
-          <select class="filters__options" onchange=${onChange}>
-            <option data-filter='{ "sortAuthorName": "none", sortBookName: "none" }'>
-              Default sorting
-            </option>
-            <option data-filter='{ "sortAuthorName": "asc", "sortBookName": "none" }'>
-              Author Name: A-Z
-            </option>
-            <option data-filter='{ "sortAuthorName": "desc", "sortBookName": "none" }'>
-              Author Name: Z-A
-            </option>
-            <option data-filter='{ "sortAuthorName": "none", "sortBookName": "asc" }'>
-              Book Name: A-Z
-            </option>
-            <option data-filter='{ "sortAuthorName": "none", "sortBookName": "desc" }'>
-              Book Name: Z-A
-            </option>
-          </select>
-        </label>
-      </div>
-
-      <div class="filters__item">
-        <label>
-          <strong class="filters__label">
             Show Genres:
           </strong>
           <select class="filters__options" onchange=${onChange}>
@@ -68,6 +43,32 @@ module.exports = (filters, send) => {
             ${filters.available.gender.map((option) => html`
               <option data-filter='{ "gender": "${option}" }'>${option}</option>
             `)}
+          </select>
+        </label>
+      </div>
+
+
+      <div class="filters__item">
+        <label>
+          <strong class="filters__label">
+            Sort by:
+          </strong>
+          <select class="filters__options" onchange=${onChange}>
+            <option data-filter='{ "sortAuthorName": "none", sortBookName: "none" }'>
+              Default sorting
+            </option>
+            <option data-filter='{ "sortAuthorName": "asc", "sortBookName": "none" }'>
+              Author Name: A-Z
+            </option>
+            <option data-filter='{ "sortAuthorName": "desc", "sortBookName": "none" }'>
+              Author Name: Z-A
+            </option>
+            <option data-filter='{ "sortAuthorName": "none", "sortBookName": "asc" }'>
+              Book Name: A-Z
+            </option>
+            <option data-filter='{ "sortAuthorName": "none", "sortBookName": "desc" }'>
+              Book Name: Z-A
+            </option>
           </select>
         </label>
       </div>

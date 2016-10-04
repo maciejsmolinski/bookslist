6c00a97b6a22c2300550f6b01f383fd62bb33760
Maciej Smolinski <contact@maciejsmolinski.com>
2016-10-04 18:49:47 +0200

    Docs: Document `date` partial since it serves pretty generic purpose.

--- src/views/partials/date.js
+++ src/views/partials/date.js
@@ -1,5 +1,21 @@
 const html = require('choo/html');
 
+/**
+ * Returns <time> tag with locally formatted date based on the given timestamp.
+ * If no timestamp is provided, current date is used.
+ *
+ * Sample Usage:
+ *
+ *   date();               // Retrieves current date
+ *   date(1475599511247);  // Renders <time> tag for specified date
+ *
+ *   // Outputs (en-US):
+ *   // <time datetime="2016-10-04T16:45:11.247Z">10/4/2016</time>
+ *   //
+ *   // Outputs (en-GB):
+ *   // <time datetime="2016-10-04T16:45:11.247Z">04/10/2016</time>
+ *
+ */
 module.exports = (timestamp = (+new Date())) => {
   const date      = (new Date(timestamp));
   const datetime  = date.toISOString();
